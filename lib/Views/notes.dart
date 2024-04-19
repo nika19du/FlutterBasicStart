c5e78e2cb6e6c76f43e45e6cd719794295078f53
nika19du <qnkovanikol29@gmail.com>
2024-04-19 21:43:54 +0300

    adding http logic

--- lib/Views/notes.dart
+++ lib/Views/notes.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/Views/add-page.dart';
+import 'package:http/http.dart' as http;
 
 class Notes extends StatefulWidget {
   const Notes({super.key});
@@ -8,10 +13,78 @@ class Notes extends StatefulWidget {
 }
 
 class _NotesState extends State<Notes> {
+
+  bool isLoading = true;
+  List items = [];
+
+  @override
+  void initState(){
+
+    super.initState();
+
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator()),
+          replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context,index) {
+                final item = items[index] as Map;
+                return ListTile(
+                  leading: CircleAvatar(child: Text('${index+1}')),
+                    title: Text(item['title']),
+                    subtitle: Text(item['description']),
+                );
+            }
+          )
+        )
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: navigateToAddPage,
+          label: Text('Add Todo')
+      ),
+    );
   }
+
+  void navigateToAddPage(){
+    final route = MaterialPageRoute(
+        builder: (context)=> AddTodoPage()
+    );
+
+    Navigator.push(context, route);
+  }
+
+  // get all api
+  Future<void> fetchTodo() async {
+    setState(() {
+      isLoading = true;
+    });
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if(response.statusCode==200){
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/Authentication/login.dart';
 import 'package:flutter_demo/Authentication/signup.dart';
+import 'package:flutter_demo/Views/notes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SignUp(),
+      home:  Notes(),
     );
   }
 }
